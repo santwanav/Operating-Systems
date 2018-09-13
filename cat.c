49c198a0e91d8dd43caf9f8b8a1c82f892cb68bd
santwana <vermasantwana@gmail.com>
2018-09-13 23:00:50 +0530

    Adds support for multiple files

--- cat.c
+++ cat.c
@@ -2,30 +2,23 @@
 #include<stdlib.h>
 /* It should accept just one argument and print an error on all other cases*/
 int main(int argc, char *argv[]) {
-	if (argc > 2) {
-		printf("Too many arguments.");
-		exit(1);
-	}
-	else if (argc < 2) {
+	if (argc < 2) {
 		printf("No file specified.");
 		exit(1);
 	}
-	else {
-		FILE *file = fopen(argv[1], "r");
+	for (int i = 1; i < argc; i++) {
+		FILE *file = fopen(argv[i], "r");
 		if (file == NULL) {
 			printf("No such file found");
 			exit(1);
 		}
-		else {
-			fseek(file, 0, SEEK_END);
-			long fsize = ftell(file);
-			fseek(file, 0, SEEK_SET);
-			char *buff = malloc(fsize+1);
-			fread(buff, fsize, 1, file);
-			fclose(file);
-			printf("%s", buff);
-			exit(0);
-		}
+		fseek(file, 0, SEEK_END);
+		long fsize = ftell(file);
+		fseek(file, 0, SEEK_SET);
+		char *buff = malloc(fsize+1);
+		fread(buff, fsize, 1, file);
+		fclose(file);
+		printf("%s", buff);
 	}
 	return 0;
 }

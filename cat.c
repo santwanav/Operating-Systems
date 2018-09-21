2090182b68a9f9b88ea69ea766ed200dcaaab637
santwana <vermasantwana@gmail.com>
2018-09-21 13:43:08 +0530

    Uses mmap to read large files

--- cat.c
+++ cat.c
@@ -1,26 +1,44 @@
 #include<stdio.h>
+#include<stdint.h>
 #include<stdlib.h>
-/* It should accept just one argument and print an error on all other cases*/
+#include<sys/mman.h>
+#include<sys/stat.h>
+#include<fcntl.h>
+#include<unistd.h>
+#define handle_error(msg) \
+	do { perror(msg); exit(EXIT_FAILURE); } while (0)
+
+
 int main(int argc, char *argv[]) {
 	if (argc < 2) {
 		printf("No file specified.");
 		exit(1);
 	}
 	for (int i = 1; i < argc; i++) {
-		FILE *file = fopen(argv[i], "r");
-		if (file == NULL) {
-			printf("No such file found");
-			exit(1);
-		}
-		fseek(file, 0, SEEK_END);
-		long fsize = ftell(file);
-		fseek(file, 0, SEEK_SET);
-		char *buff = malloc(fsize+1);
-		fread(buff, fsize, 1, file);
-		free(buff);
-		fclose(file);
-		printf("%s", buff);
+		int file = open(argv[i], O_RDONLY);
+		if (file == -1)
+			handle_error("No such file");
+		struct stat sb;
+		fstat(file, &sb);
+		printf("Size: %lu\n", (uint64_t)sb.st_size);
+		char *page = mmap(NULL, sb.st_size, PROT_READ, MAP_SHARED, file, 0);
+		if (page == MAP_FAILED) handle_error("mmap");
+		close(file);
+		for (int i = 0; i < sb.st_size; i++)
+			printf("%c", page[i]);
 	}
 	return 0;
+}
+
+void print_file(char *file) {
+	int fd = open(file, O_RDONLY);
+	if (file == -1)
+		handle_error("No such file");
+	struct stat sb;
+	fstat (file, &sb);
+	char *page == mmap(NULL, sb.st_size; PROT_READ, MAP_SHARED, file, 0);
+	if (page == MAP_FAILED) handle_error("mmap");
+	close(file)
+		printf("%c", page[i]);
 }
 

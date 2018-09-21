90feb83630fc91671425369cd884531a2bbc4810
santwana <vermasantwana@gmail.com>
2018-09-21 13:54:23 +0530

    Fixes issues with the previous commit

--- cat.c
+++ cat.c
@@ -8,37 +8,28 @@
 #define handle_error(msg) \
 	do { perror(msg); exit(EXIT_FAILURE); } while (0)
 
-
+void print_file(char* file);
 int main(int argc, char *argv[]) {
 	if (argc < 2) {
 		printf("No file specified.");
 		exit(1);
 	}
 	for (int i = 1; i < argc; i++) {
-		int file = open(argv[i], O_RDONLY);
-		if (file == -1)
-			handle_error("No such file");
-		struct stat sb;
-		fstat(file, &sb);
-		printf("Size: %lu\n", (uint64_t)sb.st_size);
-		char *page = mmap(NULL, sb.st_size, PROT_READ, MAP_SHARED, file, 0);
-		if (page == MAP_FAILED) handle_error("mmap");
-		close(file);
-		for (int i = 0; i < sb.st_size; i++)
-			printf("%c", page[i]);
+		print_file(argv[i]);
 	}
 	return 0;
 }
 
 void print_file(char *file) {
 	int fd = open(file, O_RDONLY);
-	if (file == -1)
-		handle_error("No such file");
+	if (fd == -1)
+		handle_error("cat error");
 	struct stat sb;
-	fstat (file, &sb);
-	char *page == mmap(NULL, sb.st_size; PROT_READ, MAP_SHARED, file, 0);
+	fstat (fd, &sb);
+	char *page = mmap(NULL, sb.st_size, PROT_READ, MAP_SHARED, fd, 0);
 	if (page == MAP_FAILED) handle_error("mmap");
-	close(file)
+	close(fd);
+	for (int i = 0; i < sb.st_size; i++)
 		printf("%c", page[i]);
 }
 
